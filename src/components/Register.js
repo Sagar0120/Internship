ba18716eb14a0d271780a37f8c61bfc2f44227cd
Sagar Bhanushali <80502222+Sagar0120@users.noreply.github.com>
2022-06-07 12:46:54 +0530

    update Register file

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,11 +1,38 @@
+import React ,{useState} from "react"
 function Register(){
+    const [Name, setName] = useState("")
+    const [Number, setNumber] = useState()
+    const [Password, setPassword] = useState("")
+    const [Email, setEmail] = useState("")
+
+    async function SignUp(){
+        let item = {
+            Name,
+            Number,
+            Email,
+            Password
+        }
+        console.log(item);
+        let result = await fetch("https://api.gyftm.in/api/Auth/CreateUser", {
+            method : 'POST',
+            body : JSON.stringify(item),
+            headers : {
+                "Content-Type" : 'application/json',
+                "Accept" : 'application/json'
+            }
+        })
+        result  = await result.json()
+        console.log("result", result)
+    }
+
     return(
         <div>
             <h1>Register Page</h1>
-            <input type="text" placeholder="Name"/><br />
-            <input type="password" placeholder="Password" /><br />
-            <input type="email" placeholder="Email Id"/><br />
-            <button>Sing Up</button>
+            <input type="text" value={Name} onChange = {(e)=> setName(e.target.val)} placeholder="Name"/><br />
+            <input type="number" value={Number} onChange = {(e)=> setNumber(e.target.val)} placeholder="Mobile Number"/><br />
+            <input type="email" value={Email} onChange = {(e)=> setEmail(e.target.val)} placeholder="Email Id"/><br />
+            <input type="password" value={Password} onChange = {(e)=> setPassword(e.target.val)} placeholder="Password" /><br />
+            <button onClick={SignUp}>Sing Up</button>
         </div>
     )
 }
